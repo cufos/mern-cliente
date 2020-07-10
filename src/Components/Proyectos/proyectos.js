7e1567b024d962e65400aa1d3d26978b08278c92
Juni <yunior.varon@yahoo.com>
2020-07-10 11:39:37 +0200

    Creando el sidebar de los proyectos

--- src/Components/Proyectos/proyectos.js
+++ src/Components/Proyectos/proyectos.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Sidebar from "../Layout/sidebar";
 
 const Proyectos = () => {
-  return <h1>Desde Proyectos</h1>;
+  return (
+    <div className="contenedor-app">
+      <Sidebar />
+
+      <div className="seccion-principal">
+        <main>
+          <div className="contenedor-tareas"></div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Proyectos;
